f9952983bfa403909ed466de08bd9b07e63a53e5
Ilmira <tregulovailmira@gmail.com>
2021-05-21 18:25:17 +0300

    Feature: create todos CR-D

---
+++ server/src/modules/todo/todo.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  HttpCode,
+  BadRequestException,
+} from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { TodoEntity } from './todo.entity';
+
+@Controller('users/:userId/todos')
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Post()
+  async create(
+    @Body() createTodoDto: CreateTodoDto,
+    @Param('userId') userId: string,
+  ): Promise<TodoEntity> {
+    try {
+      return await this.todoService.create(createTodoDto, +userId);
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  @Get()
+  async findAll(@Param('userId') userId: string): Promise<TodoEntity[]> {
+    return await this.todoService.findAll(userId);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<TodoEntity> {
+    return await this.todoService.findOne(+id);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  async remove(@Param('id') id: string): Promise<void> {
+    await this.todoService.remove(+id);
+  }
+}
---
+++ server/src/modules/todo/todo.service.ts
@@ -0,0 +1,58 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TodoEntity } from './todo.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class TodoService {
+  constructor(
+    @InjectRepository(TodoEntity)
+    private readonly todoRepository: Repository<TodoEntity>,
+  ) {}
+
+  public async create(
+    createTodoDto: CreateTodoDto,
+    userId: number,
+  ): Promise<TodoEntity> {
+    const newTodo = this.todoRepository.create();
+    newTodo.header = createTodoDto.header;
+    if (createTodoDto.body) {
+      newTodo.body = createTodoDto.body;
+    }
+    newTodo.userId = userId;
+
+    try {
+      return this.todoRepository.save(newTodo);
+    } catch (error) {
+      throw new BadRequestException('user with');
+    }
+  }
+
+  public async findAll(userId: string): Promise<TodoEntity[]> {
+    try {
+      return await this.todoRepository.find({ where: { userId } });
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  public async findOne(id: number): Promise<TodoEntity> {
+    const foundTodo = await this.todoRepository.findOne(id);
+    if (foundTodo) {
+      return foundTodo;
+    }
+    throw new NotFoundException('Todo with this id not found');
+  }
+
+  public async remove(id: number): Promise<void> {
+    const { affected } = await this.todoRepository.delete(id);
+    if (affected === 0) {
+      throw new BadRequestException("Can't delete user with this id");
+    }
+  }
+}
