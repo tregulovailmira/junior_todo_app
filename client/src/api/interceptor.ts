50c587ad5e6ccae729ad3b75fa86c5c8227a98a4
Ilmira <tregulovailmira@gmail.com>
2021-05-28 00:31:24 +0300

    Feature: set token to local storage

--- client/src/api/interceptor.ts
+++ client/src/api/interceptor.ts
@@ -4,4 +4,11 @@ const http = axios.create({
     baseURL: 'http://localhost:5000/'
 });
 
+http.interceptors.response.use(response => {
+    if (response.data.access_token) {
+        window.localStorage.setItem('token', response.data.access_token)
+    }
+    return response
+});
+
 export default http;
