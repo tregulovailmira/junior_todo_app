5e705688241fac83285302c96e93fad06a93dca8
Ilmira <tregulovailmira@gmail.com>
2021-05-28 17:12:16 +0300

    Feature: add status to TodoEntity

---
+++ server/src/migration/1622205817548-Todo.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Todo1622205817548 implements MigrationInterface {
+  name = 'Todo1622205817548';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TYPE "user_role_enum" AS ENUM('admin', 'user')`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "user" ("id" SERIAL NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "name" character varying NOT NULL, "role" "user_role_enum" NOT NULL DEFAULT 'user', CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TYPE "todo_status_enum" AS ENUM('done', 'in progress')`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "todo" ("id" SERIAL NOT NULL, "header" character varying NOT NULL, "body" text NOT NULL, "userId" integer NOT NULL, "status" "todo_status_enum" NOT NULL DEFAULT 'in progress', CONSTRAINT "PK_d429b7114371f6a35c5cb4776a7" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "attachment" ("id" SERIAL NOT NULL, "link" character varying NOT NULL, "todoId" integer NOT NULL, "filePath" character varying NOT NULL, CONSTRAINT "PK_d2a80c3a8d467f08a750ac4b420" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "todo" ADD CONSTRAINT "FK_1e982e43f63a98ad9918a86035c" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "attachment" ADD CONSTRAINT "FK_8802f8b09f852588593ed105493" FOREIGN KEY ("todoId") REFERENCES "todo"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "attachment" DROP CONSTRAINT "FK_8802f8b09f852588593ed105493"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "todo" DROP CONSTRAINT "FK_1e982e43f63a98ad9918a86035c"`,
+    );
+    await queryRunner.query(`DROP TABLE "attachment"`);
+    await queryRunner.query(`DROP TABLE "todo"`);
+    await queryRunner.query(`DROP TYPE "todo_status_enum"`);
+    await queryRunner.query(`DROP TABLE "user"`);
+    await queryRunner.query(`DROP TYPE "user_role_enum"`);
+  }
+}
--- server/src/modules/todo/dto/create-todo.dto.ts
+++ server/src/modules/todo/dto/create-todo.dto.ts
@@ -1,8 +1,12 @@
-import { Length } from 'class-validator';
+import { Length, IsOptional } from 'class-validator';
 
 export class CreateTodoDto {
   @Length(2, 128)
   header: string;
 
+  @IsOptional()
   body: string;
+
+  @IsOptional()
+  status: string;
 }
