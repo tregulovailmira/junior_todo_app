661b87068b401b7c802b61eeb7278c02d3ec0ada
Ilmira <tregulovailmira@gmail.com>
2021-06-01 10:19:25 +0300

    Feature: add request, reducer, payloadCreator for creating todo

--- client/src/payloadCreators/todoPayload.ts
+++ client/src/payloadCreators/todoPayload.ts
@@ -1,6 +1,18 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import * as API from '../api/restController';
 
+export const createTodoRequest = createAsyncThunk(
+  'CREATE_USER_TODO',
+  async (data: any, { rejectWithValue }) => {
+    try {
+      const { data: newTodo } = await API.createUserTodo(data);
+      return newTodo;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
+
 export const getTodosRequest = createAsyncThunk(
   'GET_USER_TODOS',
   async (_, { rejectWithValue }) => {
