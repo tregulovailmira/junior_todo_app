c29c81e3e2bb5f7c64a4963e2dbe34cf77df94c5
Ilmira <tregulovailmira@gmail.com>
2021-06-04 12:21:03 +0300

    Feature: add create user button

--- client/src/pages/AdminUsersScreen/index.tsx
+++ client/src/pages/AdminUsersScreen/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '@material-ui/core';
 import UsersTable from '../../components/UsersTable';
 function AdminUsersScreen() {
   return (
     <>
+      <Link to="/users/create" style={{ textDecoration: 'none' }}>
+        <Button color="primary" variant="contained">
+          Create user
+        </Button>
+      </Link>
+
       <UsersTable />
     </>
   );
